5cf988c3c7dc8d8627132a3fa523ef845fefabf0
bedru mekiyu <bedru.mekiyu-ug@aau.edu.et>
2026-06-07 00:41:35 +0300

    feat(routes): gate question endpoints behind participant check

--- backend/src/routes/sessionQuestionRoutes.js
+++ backend/src/routes/sessionQuestionRoutes.js
@@ -1,18 +1,18 @@
 import { Router } from "express";
 import { protect } from "../middlewares/authMiddleware.js";
-import { requireSessionRole } from "../middlewares/sessionAuth.js";
+import { requireSessionParticipant, requireSessionRole } from "../middlewares/sessionAuth.js";
 import * as questionCtrl from "../controllers/sessionQuestionController.js";
 
 const router = Router({ mergeParams: true });
 
 router.use(protect);
 
-router.post("/", questionCtrl.createQuestion);
-router.get("/", questionCtrl.getQuestions);
-router.get("/stats", questionCtrl.getQuestionStats);
-router.put("/:questionId", questionCtrl.editQuestion);
-router.delete("/:questionId", questionCtrl.deleteQuestion);
-router.post("/:questionId/upvote", questionCtrl.upvoteQuestion);
+router.post("/", requireSessionParticipant, questionCtrl.createQuestion);
+router.get("/", requireSessionParticipant, questionCtrl.getQuestions);
+router.get("/stats", requireSessionParticipant, questionCtrl.getQuestionStats);
+router.put("/:questionId", requireSessionParticipant, questionCtrl.editQuestion);
+router.delete("/:questionId", requireSessionParticipant, questionCtrl.deleteQuestion);
+router.post("/:questionId/upvote", requireSessionParticipant, questionCtrl.upvoteQuestion);
 router.post("/:questionId/answer", requireSessionRole("host", "cohost"), questionCtrl.answerQuestion);
 router.post("/:questionId/pin", requireSessionRole("host", "cohost"), questionCtrl.pinQuestion);
 router.post("/:questionId/archive", requireSessionRole("host", "cohost"), questionCtrl.archiveQuestion);
