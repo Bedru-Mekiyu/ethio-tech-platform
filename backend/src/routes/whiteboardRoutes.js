f0bc9295dc2c78af5eb3e331419c2f256f277f1f
bedru mekiyu <bedru.mekiyu-ug@aau.edu.et>
2026-06-07 00:41:58 +0300

    feat(routes): gate whiteboard read behind participant check

--- backend/src/routes/whiteboardRoutes.js
+++ backend/src/routes/whiteboardRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { protect } from "../middlewares/authMiddleware.js";
+import { requireSessionParticipant } from "../middlewares/sessionAuth.js";
 import { getWhiteboard } from "../controllers/whiteboardController.js";
 
 const router = Router({ mergeParams: true });
 
-router.get("/:sessionId/whiteboard", protect, getWhiteboard);
+router.get("/:sessionId/whiteboard", protect, requireSessionParticipant, getWhiteboard);
 
 export default router;
